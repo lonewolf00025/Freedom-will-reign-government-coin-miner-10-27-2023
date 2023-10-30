877beb74522f19e0df9de1f2276bb63946dfb342
lonewolf00025 <149115525+lonewolf00025@users.noreply.github.com>
2023-10-30 11:39:15 -0700

    Delete miners page directory

--- miners page/scripts/coin miner.js
+++
@@ -1,8 +0,0 @@
-var tcf = 0
-var cfgfm1o10 = 1 //coin fragemnts gianed form miner 1 of 10 
-var rtom1o10p1 //miner 1 timer reset 24 hour = 8640000
-var rtom1o10p2 //miner 1 timer reset 24 hour = 8640000
-var cc = 0 // cycle count
-var mbm = 1000 // mutiply by mili seconds
-
-var rtom1o10p1
