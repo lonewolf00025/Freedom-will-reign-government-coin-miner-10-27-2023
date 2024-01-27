8bfe6ad45ebbaabd5dfa5c5fa4320fe7be65fff3
lonewolf00025 <149115525+lonewolf00025@users.noreply.github.com>
2024-01-27 18:36:05 +0000

    version 0.03

--- mian/src/miners/miners.js
+++ mian/src/miners/miners.js
@@ -3,10 +3,21 @@ var numbercounter = 1
 var letter1 = "L"
 var letter2 = "A"
 var letter3 = "C"
-var cmi = []
+var cmi = {
+    level:[],
+    amount:[],
+    cost:[],
+}
 for (a = 0; a < 10; a++){
-    cmi.push(mineridbase + String(numbercounter) + letter1)
-    cmi.push(mineridbase + String(numbercounter) + letter2)
-    cmi.push(mineridbase + String(numbercounter) + letter3)
+    cmi.level.push(document.getElementById(mineridbase + String(numbercounter) + letter1).innerHTML)
+    cmi.amount.push(document.getElementById(mineridbase + String(numbercounter) + letter2).innerHTML)
+    cmi.cost.push(document.getElementById(mineridbase + String(numbercounter) + letter3).innerHTML)
     numbercounter += 1
+}
+function mineQuillar(){
+    var Quillartemp = 0
+    for(a = 0; a < 10; a++){
+        Quillartemp += level[a] * amount[a]
+    }
+    document.getElementById("Quillar").innerHTML += Quillartemp
 }
