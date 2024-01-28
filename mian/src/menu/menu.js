09f7a767733ddcaea3e93c249a1cfa95563b86e1
lonewolf00025 <149115525+lonewolf00025@users.noreply.github.com>
2024-01-28 22:44:16 +0000

    version 0.10

--- mian/src/menu/menu.js
+++ mian/src/menu/menu.js
@@ -1,16 +1,37 @@
-var menuscreencounter = 0
+var menuscreencounter = Number(document.getElementById("menuscreencounter").innerHTML)
 var menuassemblular
 var menutext = [
     "World Currency <br> Exanges",
-    "Quilar Exange into aniaml crypto coin",
+    "Quilar Exange <br> into aniaml <br> crypto coin",
     "Miners",
     "Log out",
 ]
 function menuscreen(){
-    menuassemblular = '<table><tr><td class="atd"><p class="leftarrow" onclick="leftarrow"><</p></td><td class="ctd"><div class="menutile" id="menutile" onclick="menutile"><div class="menutiletitle"><p>'+menutext[menuscreencounter]+'<p></div></div></td><td class="atd"><p class="rightarrow" onclick="rightarrow">></p></td></tr></table>'
+    menuassemblular = '<table class="menutable"><tr><td class="atd"><p class="leftarrow" onclick="leftarrow();"><</p></td><td class="ctd" onclick="menutile();"><div class="menutile" id="menutile"><div class="menutiletitle"><p>'+menutext[menuscreencounter]+'<p></div></div></td><td class="atd"><p class="rightarrow" onclick="rightarrow();">></p></td></tr></table>'
     document.getElementById("mainbody").innerHTML = menuassemblular
 }
-window.onload = function(){
+
+menuscreenonload = setInterval(function(){
     menuscreen();
+    clearInterval(menuscreenonload)
+}, 1000)
+
+function leftarrow(){
+    if(menuscreencounter === 0){
+        menuscreencounter = 3
+    }else{
+        menuscreencounter -= 1
+    }
+    menuscreen();
+    document.getElementById("menuscreencounter").innerHTML = menuscreencounter
+}
+function rightarrow(){
+    if(menuscreencounter === 3){
+        menuscreencounter = 0
+    }else{
+        menuscreencounter += 1
+    }
+    menuscreen();
+    document.getElementById("menuscreencounter").innerHTML = menuscreencounter
 }
 
--- mian/src/miners/miners.js
+++ mian/src/miners/miners.js
@@ -8,6 +8,9 @@ var cmi = {
     amount:[],
     cost:[],
 }
+
+var addtoQuilarasecond = Number(document.getElementById("quillarpersecond").innerHTML)
+
 for (a = 0; a < 10; a++){
     cmi.level.push(Number(document.getElementById(mineridbase + String(numbercounter) + letter1).innerHTML))
     cmi.amount.push(Number(document.getElementById(mineridbase + String(numbercounter) + letter2).innerHTML))
@@ -16,12 +19,28 @@ for (a = 0; a < 10; a++){
 }
 function mineQuillar(){
     var Quillartemp = 0
-    for(a = 0; a < 10; a++){
-        Quillartemp += cmi.level[a] * cmi.amount[a]
-    }
-    Quillartemp += Number(document.getElementById("Quillar").innerHTML)
+    Quillartemp = Number(document.getElementById("Quillar").innerHTML)
+    Quillartemp += addtoQuilarasecond
     document.getElementById("Quillar").innerHTML = Quillartemp
 }
 setInterval(function(){
     mineQuillar();
+    isminertileornot = Number(document.getElementById("menuscreencounter").innerHTML)
 }, 1000)
+
+var buildminers
+function buildanddisplayminers(){
+    buildminers = '<div class="minertable">'
+    for(b = 0; b < 10; b++){
+        buildminers += '<table onclick="buyMiner" class="miners"><tr><td><p>image to be added</p></td><td><p>Miner Level : '+cmi.level[b]+'</p></td><td><p>cost : '+cmi.cost[b]+'</p></td><td><p>Amount Owned : '+cmi.amount[b]+'</p></td></tr></table>'        
+    }
+    buildminers +='<button class = "menubuttonunderminers" onclick ="menuscreen();">Return to Menu</button> '
+    buildminers += '</div>'
+    document.getElementById("mainbody").innerHTML = buildminers
+}
+
+function menutile(){
+    if(isminertileornot === 2){
+        buildanddisplayminers();
+    }
+}
